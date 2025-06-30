bd263bf121d2c7fe39bc927d610137ca0df1b417
guillem.cordoba <guillem.cordoba@gmail.com>
2025-06-30 14:03:19 +0200

    Consistency

--- crates/push_notifications_service_client/src/main.rs
+++ crates/push_notifications_service_client/src/main.rs
@@ -11,6 +11,7 @@ use push_notifications_service_client::PushNotificationsServiceClient;
 use std::io::Write;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::Duration;
 use tempdir::TempDir;
 
 #[derive(Parser, Debug)]
@@ -121,6 +122,7 @@ async fn main() -> Result<()> {
             client.create_clone_request(network_seed).await?;
         }
     }
+    std::thread::sleep(Duration::from_secs(4));
 
     Ok(())
 }
