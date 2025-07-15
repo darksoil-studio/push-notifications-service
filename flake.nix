2e3ee273066928806b61e2596d34fa8fd13b74ab
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-15 13:45:17 +0200

    Attempt

--- flake.nix
+++ flake.nix
@@ -37,61 +37,8 @@ rec {
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
 
-      flake = {
-        nixosConfigurations = let
-          push_notifications_service_provider = (outputs
-            inputs).packages."x86_64-linux".push-notifications-service-provider;
-          push_notifications_service_provider_module = {
-            systemd.services.push_notifications_service_provider = {
-              enable = true;
-              path = [ push_notifications_service_provider ];
-              wantedBy = [ "multi-user.target" ];
-              serviceConfig = {
-                ExecStart =
-                  "${push_notifications_service_provider}/bin/push-notifications-service-provider --data-dir /root/push-notifications-service-provider";
-                RuntimeMaxSec = "3600"; # Restart every hour
-
-                Restart = "always";
-                RestartSec = 1;
-              };
-            };
-          };
-
-        in {
-          push_notifications_service_provider1 =
-            inputs.nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                inputs.garnix-lib.nixosModules.garnix
-                {
-                  garnix.server.persistence.name =
-                    "push-notifications-service-provider1";
-                  garnix.server.enable = true;
-                  garnix.server.persistence.enable = true;
-                  system.stateVersion = "25.05";
-                }
-                push_notifications_service_provider_module
-              ];
-            };
-          # push_notifications_service_provider2 =
-          #   inputs.nixpkgs.lib.nixosSystem {
-          #     system = "x86_64-linux";
-          #     modules = [
-          #       inputs.garnix-lib.nixosModules.garnix
-          #       {
-          #         garnix.server.persistence.name =
-          #           "push-notifications-service-provider2";
-          #         garnix.server.enable = true;
-          #         garnix.server.persistence.enable = true;
-          #         system.stateVersion = "25.05";
-          #       }
-          #       push_notifications_service_provider_module
-          #     ];
-          #   };
-        };
-      };
-
       imports = [
+        ./servers.nix
         ./workdir/happ.nix
         ./crates/push_notifications_service_provider/default.nix
         ./crates/push_notifications_service_client/default.nix
