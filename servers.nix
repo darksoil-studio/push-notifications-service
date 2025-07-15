024745377af22940ce330f2f4ab514a768a37f71
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-15 18:19:08 +0200

    Attempt

--- servers.nix
+++ servers.nix
@@ -3,13 +3,26 @@ let
   push_notifications_service_provider =
     inputs.self.outputs.packages."x86_64-linux".push-notifications-service-provider;
   push_notifications_service_provider_module = {
-    systemd.services.push_notifications_service_provider = {
+    systemd.services.push_notifications_service_provider1 = {
       enable = true;
       path = [ push_notifications_service_provider ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart =
-          "${push_notifications_service_provider}/bin/push-notifications-service-provider --data-dir /root/push-notifications-service-provider";
+          "${push_notifications_service_provider}/bin/push-notifications-service-provider --data-dir /root/push-notifications-service-provider1";
+        RuntimeMaxSec = "3600"; # Restart every hour
+
+        Restart = "always";
+        RestartSec = 1;
+      };
+    };
+    systemd.services.push_notifications_service_provider2 = {
+      enable = true;
+      path = [ push_notifications_service_provider ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart =
+          "${push_notifications_service_provider}/bin/push-notifications-service-provider --data-dir /root/push-notifications-service-provider2";
         RuntimeMaxSec = "3600"; # Restart every hour
 
         Restart = "always";
@@ -31,17 +44,6 @@ in {
           {
             garnix.server.persistence.name =
               "push-notifications-service-provider1";
-          }
-          push_notifications_service_provider_module
-        ];
-      };
-      push_notifications_service_provider2 = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          inputs.garnix-lib.nixosModules.garnix
-          {
-            garnix.server.persistence.name =
-              "push-notifications-service-provider2";
           }
           push_notifications_service_provider_module
         ];
