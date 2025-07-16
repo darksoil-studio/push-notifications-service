981bfeada8da847eeae2ac4e23a93c778fcff977
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-16 17:51:48 +0200

    Attempt

--- servers.nix
+++ servers.nix
@@ -43,6 +43,8 @@ in {
           {
             garnix.server.persistence.name =
               "push-notifications-service-provider-v0-5-x-1";
+            networking.hostName =
+              "push-notifications-service-provider-v0-5-x-1";
             system.stateVersion = "25.05";
             garnix.server.enable = true;
             garnix.server.persistence.enable = true;
@@ -57,6 +59,8 @@ in {
           push-notifications-service-provider-module
           {
             garnix.server.persistence.name =
+              "push-notifications-service-provider-v0-5-x-2";
+            networking.hostName =
               "push-notifications-service-provider-v0-5-x-2";
             system.stateVersion = "25.05";
             garnix.server.enable = true;
