6cfa1decc301645aca2bcf657487b458577ec9a9
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-16 16:46:53 +0200

    Persistence

--- servers.nix
+++ servers.nix
@@ -55,7 +55,7 @@ in {
           push-notifications-service-provider-module
           {
             garnix.server.persistence.name =
-              "push-notifications-service-provider1";
+              "push-notifications-service-provider1-v0.5.x";
             system.stateVersion = "25.05";
             garnix.server.enable = true;
             garnix.server.persistence.enable = true;
@@ -70,7 +70,7 @@ in {
           push-notifications-service-provider-module
           {
             garnix.server.persistence.name =
-              "push-notifications-service-provider2";
+              "push-notifications-service-provider2-v0.5.x";
             system.stateVersion = "25.05";
             garnix.server.enable = true;
             garnix.server.persistence.enable = true;
