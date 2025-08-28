1d38019ec444c7f764d9cff94ba6cba011cce5f6
guillem.cordoba <guillem.cordoba@gmail.com>
2025-08-28 12:29:02 +0200

    Versions

--- servers.nix
+++ servers.nix
@@ -59,7 +59,7 @@ in {
             garnix.server.enable = true;
             garnix.server.persistence.enable = true;
             garnix.server.persistence.name =
-              "push-notifications-service-provider-v0-501-1";
+              "push-notifications-service-provider-v0-502-1";
           }
         ];
       };
@@ -72,7 +72,7 @@ in {
           push-notifications-service-provider-module
           {
             garnix.server.persistence.name =
-              "push-notifications-service-provider-v0-501-2";
+              "push-notifications-service-provider-v0-502-2";
             system.stateVersion = "25.05";
             garnix.server.enable = true;
             garnix.server.persistence.enable = true;
